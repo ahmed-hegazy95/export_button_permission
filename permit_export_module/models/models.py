5e5b5e2f6aa2b5dacfdd84fd8161fc33e3f4a28a
Ahmed Hegazy <63254553+ahmed-hegazy95@users.noreply.github.com>
2020-04-10 19:31:57 +0200

    Update models.py

--- permit_export_module/models/models.py
+++ permit_export_module/models/models.py
@@ -7,7 +7,7 @@ class Hide_Button(models.AbstractModel):
     @api.model
     def fields_view_get(self, view_id=None, view_type='tree', toolbar=False, submenu=False):
         res = super(Hide_Button, self).fields_view_get(view_id=view_id, view_type=view_type,toolbar=toolbar, submenu=False)
-        group_id = self.env.user.has_group('permit_export_button.group_permit_export_button')
+        group_id = self.env.user.has_group('permit_export_module.group_permit_export_button')
         doc = etree.XML(res['arch'])
         if not group_id:
             if view_type == 'tree':
